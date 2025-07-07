e23e9ce432c846f8cc73e7db311b467deb2ac6bd
DEVANSHU PATHAK <124905708+devanshupathak02@users.noreply.github.com>
2025-07-07 12:28:16 +0530

    Time: 3 ms (89.19%), Space: 42.3 MB (14.37%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
@@ -1,28 +1,16 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        StringBuilder sb1=new StringBuilder();
-        StringBuilder sb2=new StringBuilder();
-        int count1=0;
-        int count2=0;
-        int length=(s.length())/2;
-        for(int i=0,j=length;i<length && j<s.length()+1;i++,j++){
-            char ch1= s.charAt(i);
-            char ch2=s.charAt(j);
-            if(ch1=='a'|| ch1=='e'||ch1=='i'||ch1=='o'||ch1=='u'||ch1=='A'||ch1=='E'||ch1=='I'||ch1=='O'||ch1=='U'){
-                count1++;
-            }
-            if(ch2=='a'|| ch2=='e'||ch2=='i'||ch2=='o'||ch2=='u'||ch2=='A'||ch2=='E'||ch2=='I'||ch2=='O'||ch2=='U'){
-                count2++;
-            }
-            sb1.append(ch1);
-
-            sb2.append(ch2);
-            
-        }
-        if(count1==count2){
-                return true;
-            }
-    return false;
         
+        String vow="aeiouAEIOU";
+        int mid=s.length()/2 ,ans=0;
+        for(int i=0,j=mid;i<mid && j<s.length();i++,j++){
+            if(vow.indexOf(s.charAt(i)) >=0 ){
+                ans ++;
+            } 
+            if(vow.indexOf(s.charAt(j)) >=0 ){
+                 ans --;
+            }
+        }
+        return ans==0;
     }
 }
